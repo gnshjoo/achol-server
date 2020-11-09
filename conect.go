5a07a08064bfd8218601dae1257782013afb7838
gnshjoo <gnshjoo@gmail.com>
2020-11-09 12:57:06 +0900

    auth 구현

--- conect.go
+++ conect.go
@@ -1,6 +1,12 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"encoding/xml"
+	"html/template"
+	"net/http"
+	"path/filepath"
+)
 
 type Context struct {
 	Params map[string]interface{}
@@ -12,4 +18,54 @@ type Context struct {
 type HandlerFunc func(*Context)
 
 
+// templates: 템플릿 객체를 보관하기 위한 map
+var templates = map[string]*template.Template{}
 
+
+func (c *Context) RenderJson(v interface{}){
+	// HTTP Status를 StatusOK로 지정
+	c.ResponseWriter.WriteHeader(http.StatusOK)
+	// Content-Type을 Allplication/json
+	c.ResponseWriter.Header().Set("Content-Type", "application/json;charset=utf-8")
+
+	if err := json.NewEncoder(c.ResponseWriter).Encode(v); err != nil {
+		// 에러 발생 시 RenderErr 메세드 호출
+		c.RenderErr(http.StatusInternalServerError, err)
+	}
+}
+
+func (c *Context) RenderXml(v interface{}) {
+	c.ResponseWriter.WriteHeader(http.StatusOK)
+	c.ResponseWriter.Header().Set("Content-Type", "application/json;charset=utf-8")
+
+	if err := xml.NewEncoder(c.ResponseWriter).Encode(v); err != nil {
+		c.RenderErr(http.StatusInternalServerError, err)
+	}
+
+}
+
+
+func (c *Context) RenderErr(code int, err error) {
+	if err != nil {
+		if code > 0 {
+			http.Error(c.ResponseWriter, http.StatusText(code), code)
+		} else {
+			defaltErr := http.StatusInternalServerError
+			http.Error(c.ResponseWriter, http.StatusText(defaltErr), defaltErr)
+		}
+	}
+}
+
+func (c *Context) RenderTemplate(path string, v interface{}) {
+	t, ok := templates[path]
+	if !ok {
+		t = template.Must(template.ParseFiles(filepath.Join(".", path)))
+		templates[path] = t
+
+	}
+	t.Execute(c.ResponseWriter, v)
+}
+
+func (c *Context) Redirect(url string) {
+	http.Redirect(c.ResponseWriter, c.Request, url, http.StatusMovedPermanently)
+}
--- main.go
+++ main.go
@@ -1,15 +1,77 @@
 package main
 
 import (
+	"crypto/hmac"
+	"crypto/sha1"
+	"encoding/hex"
 	"fmt"
+	"io"
+	"net/http"
+	"strings"
+	"time"
 )
 
+const VerifyMessage = "verified"
+
+func AuthHAndler(next HandlerFunc) HandlerFunc {
+	ignore := []string{"/login", "public/index.html"}
+	return func(c *Context) {
+		for _, s := range ignore {
+			if strings.HasPrefix(c.Request.URL.Path, s){
+				next(c)
+				return
+			}
+		}
+		if v, err := c.Request.Cookie("X_AUTH"); err == http.ErrNoCookie {
+			c.Redirect("/login")
+			return
+		} else if err != nil {
+			c.RenderErr(http.StatusInternalServerError, err)
+			return
+
+		} else if Verify(VerifyMessage, v.Value) {
+			next(c)
+			return
+		}
+		c.Redirect("/login")
+	}
+}
+
+func Verify(message, sig string) bool {
+	return hmac.Equal([]byte(sig), []byte(Sign(message)))
+}
+
+func CheckLogin(username, password string) bool {
+	// 로그인 처리
+	const (
+		USERNAME = "tester"
+		PASSWORD = "12345"
+	)
+	return username == USERNAME && password == PASSWORD
+}
+
+// 인증토큰 생성
+func Sign(message string) string {
+	secretKey := []byte("goland-book-secret-key2")
+	if len(secretKey) == 0 {
+		return ""
+	}
+	mac := hmac.New(sha1.New, secretKey)
+	io.WriteString(mac, message)
+	return hex.EncodeToString(mac.Sum(nil))
+}
+
 func main() {
+
+	type User struct {
+		Id string
+		Addressid string
+	}
 
 	s := NewServer()
 
 	s.HandleFunc("GET", "/", func(c *Context){
-		fmt.Fprintln(c.ResponseWriter, "welcome")
+		c.RenderTemplate("/public/index.html", map[string]interface{}{"time": time.Now()})
 	})
 
 
@@ -23,6 +85,34 @@ func main() {
 	s.HandleFunc("POST", "/users", logHandler(recoverHandler(parseFormHandler(parseJsonBodyHandler(func(c *Context) {
 		fmt.Fprintln(c.ResponseWriter, c.Params)
 	})))))
+
+	s.HandleFunc("GET", "/users/:id", func(c *Context){
+		u := User{Id: c.Params["id"].(string)}
+		c.RenderXml(u)
+	})
+
+	s.HandleFunc("GET", "/users/:user_id/addresses/:address_id", func(c *Context) {
+		u:= User{c.Params["user_id"].(string), c.Params["address_id"].(string)}
+		c.RenderJson(u)
+	})
+
+	s.HandleFunc("GET", "/login", func (c *Context) {
+		c.RenderTemplate("/public/login.html", map[string]interface{}{"message":"로그인이 필요합니다."})
+	})
+
+	s.HandleFunc("POST", "/login", func(c *Context){
+		if CheckLogin(c.Params["username"].(string), c.Params["password"].(string)) {
+			http.SetCookie(c.ResponseWriter, &http.Cookie{
+				Name: "X_AUTH",
+				Value: Sign(VerifyMessage),
+				Path: "/",
+			})
+			c.Redirect("/")
+		}
+		c.RenderTemplate("/public/login.html", map[string]interface{}{"message": "id 또는 password가 일치하지 않습니다."})
+	})
+
+	s.Use(AuthHAndler)
 
 	s.Run(":8080")
 
